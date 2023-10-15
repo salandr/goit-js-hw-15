dd39d29d2954a6983d3b681c00d78cbdb57ca42b
salandr <ansaliy200013@gmail.com>
2023-10-15 23:28:08 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,22 +1,22 @@
-const loginForm = document.querySelector('login-form');
+const loginForm = document.querySelector('.login-form');
 
 loginForm.addEventListener('submit', event => {
   event.preventDefault();
+
+  const emailInput = loginForm.elements.email;
+  const passwordInput = loginForm.elements.password;
+
+  if (!emailInput.value || !passwordInput.value) {
+    alert('Будь ласка, заповніть усі поля форми');
+    return;
+  }
+
+  const formData = {
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
+
+  console.log(formData);
+
+  loginForm.reset();
 });
-
-const emailInput = loginForm.elements.email;
-const passwordInput = loginForm.elements.password;
-
-if (!emailInput.value || !passwordInput.value) {
-  alert('Будь ласка, заповніть усі поля форми');
-  return;
-}
-
-const formData = {
-  email: emailInput.value,
-  password: passwordInput.value,
-};
-
-console.log(formData);
-
-loginForm.reset();
